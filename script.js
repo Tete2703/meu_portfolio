ea28da07dfa1c04d9e7a7d22fe579dd94c0551e9
Terezinha Fonseca <111810333+Tete2703@users.noreply.github.com>
2023-05-13 21:54:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ ativaLetra(titulo)
     
 
 const ativaMenu = document.querySelector('.fa-bars');
-
 const navMenu = document.querySelector('header .navegacao-primaria')
 ativaMenu.addEventListener('click', () => {
     ativaMenu.classList.toggle('fa-x')
